058b928c2376fbdcb97e0b25f79dc1c3ee5230ee
erickerraez <edg.erraez@yavirac.edu.ec>
2018-03-15 11:40:15 -0500

    Electricidad

--- client/src/app/layout/layout-routing.module.ts
+++ client/src/app/layout/layout-routing.module.ts
@@ -20,7 +20,8 @@ const routes: Routes = [
             { path: 'carrera-software', loadChildren: './carrera-software/carrera-software.module#CarreraSoftwareModule' },
             { path: 'investigacion', loadChildren: './investigacion/investigacion.module#InvestigacionModule' },
             { path: 'vinculacion', loadChildren: './vinculacion/vinculacion.module#VinculacionModule' },
-            { path: 'diseño-modas', loadChildren: './diseno-modas/diseno-modas.module#DisenoModasModule' }
+            { path: 'diseño-modas', loadChildren: './diseno-modas/diseno-modas.module#DisenoModasModule' },
+            { path: 'electricidad', loadChildren: './electricidad/electricidad.module#ElectricidadModule' }
         ]
     }
 ];
