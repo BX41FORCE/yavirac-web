2df86d8aa8bd4b3f2192082c2a8692446fbc9e46
erickerraez <edg.erraez@yavirac.edu.ec>
2018-03-15 10:17:44 -0500

    Diseño De Modas

--- client/src/app/layout/layout-routing.module.ts
+++ client/src/app/layout/layout-routing.module.ts
@@ -16,7 +16,8 @@ const routes: Routes = [
             { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
             { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
             { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' },
-            { path: 'bolsa-empleo', loadChildren: './bolsa-empleo/bolsa-empleo.module#BolsaEmpleoModule' }
+            { path: 'bolsa-empleo', loadChildren: './bolsa-empleo/bolsa-empleo.module#BolsaEmpleoModule' },
+            { path: 'diseño-modas', loadChildren: './diseno-modas/diseno-modas.module#DisenoModasModule' },
         ]
     }
 ];
