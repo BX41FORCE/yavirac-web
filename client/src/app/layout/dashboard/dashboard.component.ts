170f1242962c53a7471549665dc712a6099f48fe
BX41FORCE <bx41force@gmail.com>
2018-03-15 16:49:51 -0500

    Modificación index

--- client/src/app/layout/dashboard/dashboard.component.ts
+++ client/src/app/layout/dashboard/dashboard.component.ts
@@ -13,28 +13,28 @@ export class DashboardComponent implements OnInit {
     constructor() {
         this.sliders.push(
             {
-                imagePath: 'assets/images/slider1.jpg',
+                imagePath: '...',
                 label: 'First slide label',
                 text:
                     'Nulla vitae elit libero, a pharetra augue mollis interdum.'
             },
             {
-                imagePath: 'assets/images/slider2.jpg',
+                imagePath: '...',
                 label: 'Second slide label',
                 text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
             },
             {
-                imagePath: 'assets/images/slider3.jpg',
+                imagePath: '...',
                 label: 'Second slide label',
                 text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
             },
             {
-                imagePath: 'assets/images/slider4.jpg',
+                imagePath: '...',
                 label: 'Second slide label',
                 text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
             },
             {
-                imagePath: 'assets/images/slider5.jpg',
+                imagePath: '...',
                 label: 'Third slide label',
                 text:
                     'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
